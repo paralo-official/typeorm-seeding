b8e053aed97e1652911201200691f3c37dd8a01b
Moritz Hein <moritz.hein@live.de>
2023-01-16 21:47:47 +0100

    Export important library types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 export { Factory } from './factory'
 export { Seeder } from './seeder'
 export { SeedingSource } from './seeding-source'
+export type {
+  SeederOptions,
+  SeederOptionsOverrides,
+  FactoryOptions,
+  FactoryOptionsOverrides,
+  SeedingSourceOptions,
+} from './types'
