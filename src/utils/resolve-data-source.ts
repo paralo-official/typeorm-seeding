c122c1ef6e7c037e0927a282c8fb747405b92f5a
Moritz Hein <moritz.hein@live.de>
2023-01-16 22:53:05 +0100

    Fix resolveDataSource to use hasOwnProperty instead of instanceof checks to work with pnpm

--- src/utils/resolve-data-source.ts
+++ src/utils/resolve-data-source.ts
@@ -1,8 +1,12 @@
 import { DataSource, DataSourceOptions } from 'typeorm'
 
+export function isDataSource(obj: unknown): obj is DataSource {
+  return typeof obj === 'object' && !!obj?.hasOwnProperty('isInitialized')
+}
+
 export const resolveDataSource = (dataSource: DataSource | DataSourceOptions): DataSource => {
   // create new instance if necessary
-  const dataSourceToReturn = dataSource instanceof DataSource ? dataSource : new DataSource(dataSource)
+  const dataSourceToReturn = isDataSource(dataSource) ? dataSource : new DataSource(dataSource)
   // return the data source
   return dataSourceToReturn
 }
